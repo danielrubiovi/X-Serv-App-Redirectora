591ccb4005b16835c7da1860c070dd64ca7db9b8
Daniel Rubio Villegas <DanielRubio@MacBook-Pro-de-Daniel-2.local>
2016-03-10 09:55:31 +0100

    entrega redirecciones

---
+++ webapp-redirect.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+"""
+webApp class
+ Root for hierarchy of classes implementing web applications
+
+ Copyright Jesus M. Gonzalez-Barahona and Gregorio Robles (2009-2015)
+ jgb @ gsyc.es
+ TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
+ October 2009 - February 2015
+"""
+
+import socket
+import random
+
+
+class webApp:
+    """Root of a hierarchy of classes implementing web applications
+
+    This class does almost nothing. Usually, new classes will
+    inherit from it, and by redefining "parse" and "process" methods
+    will implement the logic of a web application in particular.
+    """
+
+    def parse(self, request):
+        """Parse the received request, extracting the relevant information."""
+
+        return None
+
+    def process(self, parsedRequest):
+        """Process the relevant elements of the request.
+
+        Returns the HTTP code for the reply, and an HTML page.
+        """
+        ranNum = str(random.randint(0,10000))
+        url = 'http://localhost:1234/' + ranNum
+        htmlAnswer = "<html><body>"
+        htmlAnswer += "<p><h5>"'Estas siendo redirigido a: '+ str(url)+ "</h5></p>"
+        htmlAnswer += "Redirect <meta http-equiv='refresh' content='3;"
+        htmlAnswer += "URL="
+        htmlAnswer += ranNum
+        htmlAnswer += "'></p>"
+        htmlAnswer += "</body></html>"
+        return ("302 Moved Temporarily", htmlAnswer)
+
+    def __init__(self, hostname, port):
+        """Initialize the web application."""
+
+        # Create a TCP objet socket and bind it to a port
+        mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        mySocket.bind((hostname, port))
+
+        # Queue a maximum of 5 TCP connection requests
+        mySocket.listen(5)
+
+        # Accept connections, read incoming data, and call
+        # parse and process methods (in a loop)
+
+        while True:
+            print 'Waiting for connections'
+            (recvSocket, address) = mySocket.accept()
+            print 'HTTP request received (going to parse and process):'
+            request = recvSocket.recv(2048)
+            print request
+            parsedRequest = self.parse(request)
+            (returnCode, htmlAnswer) = self.process(parsedRequest)
+            print 'Answering back...'
+            recvSocket.send("HTTP/1.1 " + returnCode + " \r\n\r\n"
+                            + htmlAnswer + "\r\n")
+            recvSocket.close()
+
+if __name__ == "__main__":
+    testWebApp = webApp("localhost", 1234)
